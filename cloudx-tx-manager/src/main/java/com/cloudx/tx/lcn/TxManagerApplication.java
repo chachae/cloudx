b19016772d14e0ad5881e0300994e1df5b49421f
chachae <chenyuexin1998@gmail.com>
2020-06-01 00:36:54 +0800

    feat: 增加 TX-LCN 分布式事务控制系统

---
+++ cloudx-tx-manager/src/main/java/com/cloudx/tx/lcn/TxManagerApplication.java
@@ -0,0 +1,21 @@
+package com.cloudx.tx.lcn;
+
+import com.codingapi.txlcn.tm.config.EnableTransactionManagerServer;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+/**
+ * @author chachae
+ * @since 2020/5/1 11:31
+ */
+@SpringBootApplication
+@EnableTransactionManagerServer
+public class TxManagerApplication {
+
+  public static void main(String[] args) {
+    new SpringApplicationBuilder(TxManagerApplication.class)
+        .web(WebApplicationType.SERVLET)
+        .run(args);
+  }
+}
---
+++ cloudx-tx-manager/src/main/java/com/cloudx/tx/lcn/init/StartedUpRunner.java
@@ -0,0 +1,42 @@
+package com.cloudx.tx.lcn.init;
+
+import java.time.LocalDateTime;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author chachae
+ * @since 2020/04/25
+ */
+@Component
+public class StartedUpRunner implements ApplicationRunner {
+
+  private final ConfigurableApplicationContext context;
+  private final Environment environment;
+
+  @Autowired
+  public StartedUpRunner(ConfigurableApplicationContext context, Environment environment) {
+    this.context = context;
+    this.environment = environment;
+  }
+
+  private static void printSystemUpBanner(Environment environment) {
+    String banner = "-----------------------------------------\n" +
+        "server up success,current datetime is：" + LocalDateTime.now() + "\n" +
+        "server application name：" + environment.getProperty("spring.application.name") + "\n" +
+        "server port：" + environment.getProperty("server.port") + "\n" +
+        "-----------------------------------------";
+    System.out.println(banner);
+  }
+
+  @Override
+  public void run(ApplicationArguments args) {
+    if (context.isActive()) {
+      printSystemUpBanner(environment);
+    }
+  }
+}
